a78f67c5b990600bf62943498aeaa7077935bdeb
Ch3shireDev <thesmilingcatofcheshire@gmail.com>
2017-03-26 04:00:59 +0200

    Some progress
    Added evolution of exp(i*theta) component, still problems with making it
    running.

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ import matplotlib.pyplot as plt
 
 Nx, Ny, Nz = 2048, 256, 8
 Lx, Ly, Lz = 10., 10., 1.
-dt = 0.0001
+dt = 0.001
 
 dx = Lx/Nx
 
-N = 500
 
 print Nx
 
 SpaceX = np.linspace(-Lx/2, Lx/2 , Nx)
 tab = np.exp(-SpaceX**2)
 tab /= np.sqrt(np.linalg.norm(tab))
-tab += 1
+tab += 0.9
 
 tab = tab.astype(np.complex)
 tab2 = tab.copy()
@@ -24,40 +23,42 @@ tab2 = tab.copy()
 Kx = np.arange(Nx) - np.append(np.zeros(Nx/2), np.ones(Nx/2)*Nx)
 Kx *= 2*np.pi/Lx
 
-K2 = np.exp(-1j*Kx**2*dt)
+K2 = np.exp(1j*Kx**2*dt)
 
 Tab = []
 Tab2 = []
 
-E = np.ones(Nx)
+
+
+
+N = 200
+E = np.ones(Nx).astype(np.complex)
+
+r = np.abs(tab2)
 
 for i in range(N):
     tab = ifft(K2*fft(tab))
-    E = tab/np.abs(tab)
-    dTheta = np.imag(np.roll(E, 1)/E - 1)/dx
+
+    # E = tab/np.abs(tab)
+    dTheta = np.real(1j*(1-np.roll(E, 1)/E))/dx
     d2Theta = (dTheta - np.roll(dTheta, 1))/dx
 
-    r = np.abs(tab2)
     dr = (r - np.roll(r, 1))/dx
     d2r = (dr - np.roll(dr, 1))/dx
 
-    # tab2 = np.exp(-1j*dt*((dTheta)**2 - 1j*d2Theta))*tab2
-    #i don't know why but without 1j thing it's more accurate
-    # dPsi = -(d2r + 2j*dTheta*dr)*E
-    #again, when I remove j it coverges better
-    tab2 = np.exp(-1j*dt*((dTheta)**2 - d2Theta))*tab2
-    dPsi = d2r*E*1j*dt
-    tab2 += dPsi
+    #i dPsi = (dTheta**2-id2Theta)*Psi - (d2r + 2i dTheta*dR)*E
 
+    r = np.exp(-(2*dTheta*dr/r + d2Theta)*dt)*r
+    # E = (r+1j*(dTheta**2*r)*dt)/np.sqrt(r**2 + dTheta**4*r**2)*E
+    E = tab/np.abs(tab)
 
+    tab2 = r*E
 
-    Tab.append(np.abs(tab))
-    Tab2.append(tab2)
+    Tab.append(np.angle(E))
+
     print i, np.linalg.norm(tab - tab2)
 
-
 exit(0)
-
 Tab = np.array(Tab)
 plt.imshow(Tab.T, cmap='gnuplot')
 plt.show()
